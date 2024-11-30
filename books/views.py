4a78c38057dd6695c6c578c4bea37348636bd891
Žan Mencigar <mencigarzan@gmail.com>
2024-11-30 16:30:32 +0100

    Added book api

--- books/views.py
+++ books/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Book
+from .serializers import BookSerializer
+
+
+class UserViewSet(viewsets.ViewSet):
+    def list(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        book = Book.objects.get(id=pk)
+        serializer = BookSerializer(book)
+
+        return Response(serializer.data)
+
+
+    def create(self, request):
+        serializer = BookSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        book = Book.objects.get(id=pk)
+        serializer = BookSerializer(instance=book, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    def destroy(self, request, pk=None):
+        book = Book.objects.get(id=pk)
+        book.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
